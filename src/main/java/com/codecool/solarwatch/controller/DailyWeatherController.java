6884298f865e6c4428e8ce4cb1794b44fce232f7
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-10-23 18:25:30 +0200

    add DailyWeatherController

---
+++ src/main/java/com/codecool/solarwatch/controller/DailyWeatherController.java
@@ -0,0 +1,29 @@
+package com.codecool.solarwatch.controller;
+
+import com.codecool.solarwatch.model.weather.DailyWeatherDTO;
+import com.codecool.solarwatch.service.DailyWeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping ("/")
+public class DailyWeatherController {
+  DailyWeatherService dailyWeatherService;
+  
+  @Autowired
+  public DailyWeatherController(DailyWeatherService dailyWeatherService) {
+    this.dailyWeatherService = dailyWeatherService;
+  }
+  
+  @GetMapping ("/")
+  public ResponseEntity<DailyWeatherDTO> getDailyWeather(@RequestParam String city,
+          @RequestParam (required = false) LocalDate date) {
+    return ResponseEntity.ok().body(dailyWeatherService.getDailyWeatherReport(city, date));
+  }
+}
