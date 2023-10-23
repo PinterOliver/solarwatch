0d6d652d726fa0f6b553305bb8cf7a1490883a9c
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-10-23 20:20:15 +0200

    add DailyWeatherService

---
+++ src/main/java/com/codecool/solarwatch/service/DailyWeatherService.java
@@ -0,0 +1,48 @@
+package com.codecool.solarwatch.service;
+
+import com.codecool.solarwatch.exception.BadAPIRequestException;
+import com.codecool.solarwatch.externalApi.GeocodingAPI;
+import com.codecool.solarwatch.externalApi.SunTimeAPI;
+import com.codecool.solarwatch.model.auxiliary.Coordinates;
+import com.codecool.solarwatch.model.weather.DailyWeatherDTO;
+import com.codecool.solarwatch.model.weather.SunTimeDetail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+public class DailyWeatherService {
+  private final Logger logger;
+  private final GeocodingAPI geocodingAPI;
+  private final SunTimeAPI sunTimeAPI;
+  
+  @Autowired
+  public DailyWeatherService(GeocodingAPI geocodingAPI, SunTimeAPI sunTimeAPI) {
+    this.geocodingAPI = geocodingAPI;
+    this.sunTimeAPI = sunTimeAPI;
+    logger = LoggerFactory.getLogger(DailyWeatherService.class);
+  }
+  
+  public DailyWeatherDTO getDailyWeatherReport(String city, LocalDate date) {
+    if (date == null) {
+      date = LocalDate.now();
+    }
+    Optional<Coordinates> coordinates = geocodingAPI.getCoordinates(city);
+    if (coordinates.isEmpty()) {
+      throw new BadAPIRequestException();
+    }
+    Optional<SunTimeDetail> sunTimeDetail = sunTimeAPI.getSunTime(coordinates.get(), date);
+    if (sunTimeDetail.isEmpty()) {
+      throw new BadAPIRequestException();
+    }
+    return getDailyWeatherDTO(city, date, sunTimeDetail.get());
+  }
+  
+  private DailyWeatherDTO getDailyWeatherDTO(String city, LocalDate date, SunTimeDetail sunTimeDetail) {
+    return new DailyWeatherDTO(city, date, sunTimeDetail.sunrise(), sunTimeDetail.sunset());
+  }
+}
